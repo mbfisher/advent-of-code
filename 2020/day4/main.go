249583ac38aa353ffcde39f312d1d8b5080a50a3
Mike Fisher <mike.fisher@skyscanner.net>
2020-12-06 19:49:32 +0000

    2020 Day 4 Part 2

--- 2020/day4/main.go
+++ 2020/day4/main.go
@@ -3,34 +3,68 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/go-playground/validator/v10"
 	"log"
 	"os"
+	"reflect"
+	"strconv"
 	"strings"
 )
 
-type PassportVisitor func(map[string]bool)
+type Height struct {
+	value string
+}
+
+type Passport struct {
+	Byr int    `validate:"required,gte=1920,lte=2002"`
+	Iyr int    `validate:"required,gte=2010,lte=2020"`
+	Eyr int    `validate:"required,gte=2020,lte=2030"`
+	Hgt Height `validate:"required"`
+	Hcl string `validate:"required,hexcolor"`
+	Ecl string `validate:"required,oneof=amb blu brn gry grn hzl oth"`
+	Pid string `validate:"required,len=9,containsany=0123456789"`
+	Cid string
+}
+
+type PassportVisitor func(Passport)
 
 func Parse(scanner *bufio.Scanner, visitor PassportVisitor) error {
-	var passport map[string]bool
+	var fields map[string]string
+
+	visit := func() {
+		byr, _ := strconv.Atoi(fields["byr"])
+		iyr, _ := strconv.Atoi(fields["iyr"])
+		eyr, _ := strconv.Atoi(fields["eyr"])
+		passport := Passport{
+			Byr: byr,
+			Iyr: iyr,
+			Eyr: eyr,
+			Hgt: Height{fields["hgt"]},
+			Hcl: fields["hcl"],
+			Ecl: fields["ecl"],
+			Pid: fields["pid"],
+			Cid: fields["cid"],
+		}
+		visitor(passport)
+	}
 
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		if passport == nil {
-			passport = make(map[string]bool)
+		if fields == nil {
+			fields = make(map[string]string)
 		}
 
 		if line != "" {
 			pairs := strings.Split(line, " ")
 			for _, pair := range pairs {
 				kv := strings.Split(pair, ":")
-				passport[kv[0]] = true
+				fields[kv[0]] = kv[1]
 			}
 		} else {
-			visitor(passport)
-			passport = nil
+			visit()
+			fields = nil
 		}
-
 	}
 
 	if err := scanner.Err(); err != nil {
@@ -38,7 +72,7 @@ func Parse(scanner *bufio.Scanner, visitor PassportVisitor) error {
 	}
 
 	// Emit the last passport - we don't get an empty line for it
-	visitor(passport)
+	visit()
 
 	return nil
 }
@@ -54,6 +88,14 @@ var RequiredFields = []string{
 	"cid",
 }
 
+func ValidateHeight(field reflect.Value) interface{} {
+	if value, ok := field.Interface().(string); ok {
+		fmt.Println(value)
+	}
+
+	return nil
+}
+
 type passportValidator struct {
 	numValid int
 }
@@ -62,18 +104,70 @@ func (v *passportValidator) NumValid() int {
 	return v.numValid
 }
 
-func (v *passportValidator) Visitor() PassportVisitor {
-	return func(passport map[string]bool) {
-		var missingFields []string
-		for _, field := range RequiredFields {
-			if _, ok := passport[field]; !ok {
-				missingFields = append(missingFields, field)
-			}
+func (v *passportValidator) validateHeight(field reflect.Value) interface{} {
+	if hgt, ok := field.Interface().(Height); ok {
+		if len(hgt.value) < 4 {
+			return nil
 		}
 
-		if len(missingFields) == 0 || len(missingFields) == 1 && missingFields[0] == "cid" {
-			v.numValid++
+		value, _ := strconv.Atoi(hgt.value[:len(hgt.value)-2])
+		unit := hgt.value[len(hgt.value)-2:]
+
+		if unit == "cm" && value >= 150 && value <= 193 {
+			return hgt.value
 		}
+
+		if unit == "in" && value >= 59 && value <= 76 {
+			return hgt.value
+		}
+
+		return nil
+	}
+
+	return nil
+}
+
+func (v *passportValidator) Validate(passport Passport) error {
+	validate := validator.New()
+	validate.RegisterCustomTypeFunc(v.validateHeight, Height{})
+	return validate.Struct(&passport)
+}
+
+func (v *passportValidator) Visitor() PassportVisitor {
+	return func(passport Passport) {
+		//var missingFields []string
+		//isValid := true
+		//
+		//if val, ok := passport["byr"]; ok {
+		//	byr, err := strconv.Atoi(val)
+		//	if err != nil {
+		//		fmt.Printf("byr: '%s' is not a number\n", val)
+		//	} else if byr < 1920 || byr > 2002 {
+		//		fmt.Printf("byr: '%d' out of range\n", byr)
+		//	} else {
+		//		isValid = true
+		//	}
+		//} else {
+		//	missingFields = append(missingFields, "byr")
+		//}
+		//
+		//if isValid {
+		//	v.numValid++
+		//}
+
+		err := v.Validate(passport)
+
+		if err == nil {
+			v.numValid++
+		} else {
+			validationErrors := err.(validator.ValidationErrors)
+			fmt.Println(passport)
+			fmt.Println(validationErrors)
+		}
+
+		//if len(missingFields) == 0 || len(missingFields) == 1 && missingFields[0] == "cid" {
+		//	v.numValid++
+		//}
 	}
 }
 
@@ -83,7 +177,7 @@ func NewPassportValidator() *passportValidator {
 	}
 }
 
-func Part1() {
+func Part1() int {
 	wd, _ := os.Getwd()
 	file, err := os.Open(wd + "/day4/input.txt")
 	if err != nil {
@@ -100,13 +194,13 @@ func Part1() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(validator.NumValid())
+	return validator.NumValid()
 }
 
 func Part2() {
 }
 
 func main() {
-	Part1()
+	fmt.Println(Part1())
 	Part2()
 }
--- 2020/day4/main_test.go
+++ 2020/day4/main_test.go
@@ -23,7 +23,7 @@ iyr:2011 ecl:brn hgt:59in`
 func TestParse(t *testing.T) {
 	numPassports := 0
 	input := bufio.NewScanner(strings.NewReader(exampleInput))
-	err := Parse(input, func(p map[string]bool) {
+	err := Parse(input, func(p Passport) {
 		numPassports++
 	})
 
@@ -50,4 +50,99 @@ func TestPassportValidator(t *testing.T) {
 	if validPassports != 2 {
 		t.Errorf("expected %d, got %d", 2, validPassports)
 	}
+}
+
+var invalidExamples = `eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007`
+
+var validExamples = `pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719`
+
+func TestValidExamples(t *testing.T) {
+	validator := NewPassportValidator()
+	input := bufio.NewScanner(strings.NewReader(validExamples))
+
+	err := Parse(input, validator.Visitor())
+
+	if err != nil {
+		t.Errorf("got error; %v", err)
+	}
+
+	validPassports := validator.NumValid()
+	if validPassports != 4 {
+		t.Errorf("expected %d, got %d", 4, validPassports)
+	}
+}
+
+func TestInvalidExamples(t *testing.T) {
+	validator := NewPassportValidator()
+	input := bufio.NewScanner(strings.NewReader(invalidExamples))
+
+	err := Parse(input, validator.Visitor())
+
+	if err != nil {
+		t.Errorf("got error; %v", err)
+	}
+
+	validPassports := validator.NumValid()
+	if validPassports != 0 {
+		t.Errorf("expected %d, got %d", 0, validPassports)
+	}
+}
+
+func TestPart1(t *testing.T) {
+	result := Part1()
+	if result != 237 {
+		t.Errorf("expected %d, got %d", 237, result)
+	}
+}
+
+func TestValidationRules(t *testing.T) {
+	p := Passport{
+		Hgt: Height{"170"},
+		Pid: "087499704",
+		Ecl: "grn",
+		Iyr: 2012,
+		Eyr: 2030,
+		Byr: 1980,
+		Hcl: "#623a2f",
+	}
+
+	err := (&passportValidator{}).Validate(p)
+
+	if err == nil {
+		t.Errorf("wanted an error")
+	}
+	t.Log(err)
+
+	p.Hgt = Height{"170cm"}
+
+	err = (&passportValidator{}).Validate(p)
+
+	if err != nil {
+		t.Errorf("got an error")
+	}
+
+	t.Log(err)
 }
