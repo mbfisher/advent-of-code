ce4f07d08815ca10bd4ddba07e9a158168ef9a08
Mike Fisher <mike.fisher@skyscanner.net>
2020-12-07 11:34:43 +0000

    2020 Day 6 Part 2

--- 2020/day6/main_test.go
+++ 2020/day6/main_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestCountGroup(t *testing.T) {
+func TestCountGroupAnyone(t *testing.T) {
 	var tests = []struct{
 		group []string
 		want int
@@ -15,7 +15,27 @@ func TestCountGroup(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		got := countGroup(tt.group)
+		got := countGroupAnyone(tt.group)
+		if got != tt.want {
+			t.Errorf("got %d want %d", got, tt.want)
+		}
+	}
+}
+
+func TestCountGroupEveryone(t *testing.T) {
+	var tests = []struct{
+		group []string
+		want int
+	}{
+		{[]string{"abc"}, 3},
+		{[]string{"a", "b", "c"}, 0},
+		{[]string{"ab", "ac"}, 1},
+		{[]string{"a", "a", "a", "a"}, 1},
+		{[]string{"b"}, 1},
+	}
+
+	for _, tt := range tests {
+		got := countGroupEveryone(tt.group)
 		if got != tt.want {
 			t.Errorf("got %d want %d", got, tt.want)
 		}
