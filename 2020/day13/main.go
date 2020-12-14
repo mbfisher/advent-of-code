14153af4a631f3fed0d8f9596765b24d384dd16d
Mike Fisher <mike.fisher@skyscanner.net>
2020-12-14 15:02:12 +0000

    2020 Day 13 Part 2 WIP

--- 2020/day13/main.go
+++ 2020/day13/main.go
@@ -11,8 +11,8 @@ import (
 )
 
 func getFirstDepartureAfterTimestamp(timestamp int64, bus int) int64 {
-	d := math.Ceil(float64(timestamp) / float64(bus))
-	return int64(float64(bus) * d)
+	d := int64(math.Ceil(float64(timestamp) / float64(bus)))
+	return int64(bus) * d
 }
 
 func findEarliestBus(input string) int {
@@ -42,47 +42,46 @@ func findEarliestBus(input string) int {
 	return bus * int(min - timestamp)
 }
 
-func findWeirdTimestamp(input string, start int64) int64 {
+func mmi(y int64, n int64) int64 {
+	var z int64
+	for i := int64(0); i < n; i++ {
+		if (y * i) % n == 1 {
+			z = i
+			break
+		}
+	}
+	return z
+}
+
+func findWeirdTimestamp(input string) int64 {
 	lines := strings.Split(input, "\n")
 
-	buses := make(map[int]int)
+	buses := make(map[int64]int64)
 	for i, id := range strings.Split(lines[1], ",") {
 		if id == "x" {
 			continue
 		}
 		num, _ := strconv.Atoi(id)
-		buses[i] = num
+		buses[int64(i + 1)] = int64(num)
 	}
 
-	i := start
-	var ts int64
-	for true {
-		ts = int64(buses[0]) * i
-		good := true
-
-		for pos, id := range buses {
-			if pos == 0 {
-				continue
-			}
-
-			next := getFirstDepartureAfterTimestamp(ts, id)
-
-			if next - ts != int64(pos) {
-				good = false
-				break
-			}
-		}
-
-		if good {
-			break
-		}
-
-		i++
+	// https://brilliant.org/wiki/chinese-remainder-theorem/
+	// x ≡ pos (mod id)
+	N := int64(1)
+	for _, n := range buses {
+		N *= n
 	}
 
-	fmt.Println(i)
-	return ts
+	result := int64(0)
+	for n, a := range buses {
+		y := N/n
+		z := mmi(y, n)
+		result += a * y * z
+	}
+
+	return result
 }
+
 
 func Part1() int {
 	wd, _ := os.Getwd()
@@ -103,7 +102,7 @@ func Part2() int64 {
 	}
 
 	input := string(dat)
-	return findWeirdTimestamp(input, 100000000000000)
+	return findWeirdTimestamp(input)
 }
 
 func main() {
