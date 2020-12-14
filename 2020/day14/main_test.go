7206791207b927baeb77600c67bd189d572b1b33
Mike Fisher <mike.fisher@skyscanner.net>
2020-12-14 12:36:23 +0000

    2020 Day 14 Part 2

--- 2020/day14/main_test.go
+++ 2020/day14/main_test.go
@@ -11,10 +11,24 @@ mem[8] = 11
 mem[7] = 101
 mem[8] = 0`
 
-func TestSumValues(t *testing.T) {
+func TestVersion1(t *testing.T) {
 	scanner := bufio.NewScanner(strings.NewReader(exampleInput))
-	result := sumValues(scanner)
+	result := version1(scanner)
 	if result != 165 {
-		t.Fatalf("got %d want %d", result, 25)
+		t.Fatalf("got %d want %d", result, 165)
+	}
+}
+
+
+var exampleInputV2 = `mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1`
+
+func TestVersion2(t *testing.T) {
+	scanner := bufio.NewScanner(strings.NewReader(exampleInputV2))
+	result := version2(scanner)
+	if result != 208 {
+		t.Fatalf("got %d want %d", result, 208)
 	}
 }
