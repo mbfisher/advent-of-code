0ad2ec2b57ac0e8f16f5a21de03a0db88c8304d9
Mike Fisher <mike.fisher@skyscanner.net>
2021-12-02 08:47:39 +0000

    2021 Day 2 Part 1

---
+++ 2021/day2/day2.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+from typing import List
+
+
+def part1(input: str) -> int:
+    position = 0
+    depth = 0
+
+    for instruction in input.split("\n"):
+        direction, magnitude = instruction.split(" ")
+
+        if direction == "forward":
+            position += int(magnitude)
+        if direction == "up":
+            depth -= int(magnitude)
+        if direction == "down":
+            depth += int(magnitude)
+
+    return position * depth
+
+
+def part2(input: str) -> int:
+    pass
+
+
+if __name__ == '__main__':
+    example = '\n'.join([
+        "forward 5",
+        "down 5",
+        "forward 8",
+        "up 3",
+        "down 8",
+        "forward 2"
+    ])
+
+    assert part1(example) == 150
+    print(part1(Path('./input.txt').read_text()))
+
+    # assert part2(example) == 5
+    # print(part2(Path('./input.txt').read_text()))
