84c903f08b48bae030e1027368a8461caa445a9a
Mike Fisher <mfisher@gocardless.com>
2023-12-06 14:03:22 +0000

    2023 day6 part1

---
+++ 2023/day6/day6.py
@@ -0,0 +1,46 @@
+import unittest
+from pathlib import Path
+
+
+def part1(input: str) -> int:
+    lines = input.split("\n")
+    times = list(map(int, filter(len, lines.pop(0)[len("Time:"):].split(" "))))
+    records = list(map(int, filter(len, lines.pop(0)[len("Distance:"):].split(" "))))
+
+    result = 1
+
+    for i in range(len(times)):
+        time = times[i]
+        record = records[i]
+
+        wins = 0
+
+        for t in range(time):
+            distance = (time - t) * t
+            if distance > record:
+                wins += 1
+
+        result *= wins
+
+    return result
+
+
+def part2(input: str) -> int:
+    return 0
+
+
+class Test(unittest.TestCase):
+    example1 = '\n'.join([
+        "Time:      7  15   30",
+        "Distance:  9  40  200"
+    ])
+
+    def test_part_1(self):
+        self.assertEqual(288, part1(Test.example1))
+
+        print(part1(Path('./input.txt').read_text()))
+
+    def test_part_2(self):
+        self.assertEqual(0, part2(Test.example1))
+
+        print(part2(Path('./input.txt').read_text()))
---
+++ template.py
@@ -0,0 +1,24 @@
+import unittest
+from pathlib import Path
+
+
+def part1(input: str) -> int:
+    return 0
+
+
+def part2(input: str) -> int:
+    return 0
+
+
+class Test(unittest.TestCase):
+    example1 = '\n'.join([])
+
+    def test_part_1(self):
+        self.assertEqual(0, part1(Test.example1))
+
+        print(part1(Path('./input.txt').read_text()))
+
+    def test_part_2(self):
+        self.assertEqual(0, part2(Test.example1))
+
+        print(part2(Path('./input.txt').read_text()))
