f462a560ffdeef0958cfcd9e3153c10d0b21849e
Mike Fisher <mfisher@gocardless.com>
2023-12-06 14:08:20 +0000

    2023 day6 part2

--- 2023/day6/day6.py
+++ 2023/day6/day6.py
@@ -4,19 +4,19 @@ from pathlib import Path
 
 def part1(input: str) -> int:
     lines = input.split("\n")
-    times = list(map(int, filter(len, lines.pop(0)[len("Time:"):].split(" "))))
+    durations = list(map(int, filter(len, lines.pop(0)[len("Time:"):].split(" "))))
     records = list(map(int, filter(len, lines.pop(0)[len("Distance:"):].split(" "))))
 
     result = 1
 
-    for i in range(len(times)):
-        time = times[i]
+    for i in range(len(durations)):
+        duration = durations[i]
         record = records[i]
 
         wins = 0
 
-        for t in range(time):
-            distance = (time - t) * t
+        for t in range(duration):
+            distance = (duration - t) * t
             if distance > record:
                 wins += 1
 
@@ -26,7 +26,18 @@ def part1(input: str) -> int:
 
 
 def part2(input: str) -> int:
-    return 0
+    lines = input.split("\n")
+    duration = int("".join(filter(len, lines.pop(0)[len("Time:"):].split(" "))))
+    record = int("".join(filter(len, lines.pop(0)[len("Distance:"):].split(" "))))
+
+    result = 0
+
+    for t in range(duration):
+        distance = (duration - t) * t
+        if distance > record:
+            result += 1
+
+    return result
 
 
 class Test(unittest.TestCase):
@@ -41,6 +52,6 @@ class Test(unittest.TestCase):
         print(part1(Path('./input.txt').read_text()))
 
     def test_part_2(self):
-        self.assertEqual(0, part2(Test.example1))
+        self.assertEqual(71503, part2(Test.example1))
 
         print(part2(Path('./input.txt').read_text()))
