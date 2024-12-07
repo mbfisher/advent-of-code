f955955c147e9014a5f8ee2b8016f7bb25e1bc4d
Mike Fisher <mfisher@gocardless.com>
2024-12-07 15:08:17 +0000

    2023 day 12

--- 2023/day12/day12.py
+++ 2023/day12/day12.py
@@ -2,12 +2,13 @@ import re
 import unittest
 from functools import cache
 from inspect import cleandoc
+from itertools import permutations
 from pathlib import Path
-from typing import Tuple
+from typing import Tuple, Iterator
 
+def recurse(string: str, groups: Tuple[int, ...], stats) -> (int, int):
+    stats['invocations'] += 1
 
-@cache
-def recurse(string: str, groups: Tuple[int, ...]) -> int:
     wildcard = string.find('?')
 
     if wildcard == -1:
@@ -19,20 +20,60 @@ def recurse(string: str, groups: Tuple[int, ...]) -> int:
         return 0
 
     return (
-        recurse(string[:wildcard] + '.' + string[wildcard + 1:], groups) +
-        recurse(string[:wildcard] + '#' + string[wildcard + 1:], groups)
+        recurse(string[:wildcard] + '.' + string[wildcard + 1:], groups, stats) +
+        recurse(string[:wildcard] + '#' + string[wildcard + 1:], groups, stats)
     )
 
 
-def arrangements(row: str) -> int:
+def count_arrangements_recursive(row: str) -> int:
     springs, groups = row.split(" ")
     groups = tuple(map(int, groups.split(",")))
 
-    return recurse(springs, groups)
+    stats = {'invocations': 0}
+    result = recurse(springs, groups, stats)
+    # print(row, stats)
+
+    return result
+
+
+def findall(char, string) -> Iterator[int]:
+    i = string.find(char)
+    while i > -1:
+        yield i
+        i = string.find(char, i + 1)
+
+
+def count_arrangements_permutations(row: str) -> int:
+    conditions, groups = row.split(" ")
+    groups = tuple(map(int, groups.split(",")))
+
+    unknowns = tuple(findall('?', conditions))
+
+    existing_springs = conditions.count('#')
+    springs_to_place = sum(groups) - existing_springs
+
+    result = 0
+
+    init = ('#' * springs_to_place) + ('.' * (conditions.count('?') - springs_to_place))
+    all_perms = list(permutations(init))
+    perms = set(all_perms)
+    print(row, len(perms), len(all_perms))
+
+    for perm in perms:
+        arrangement = conditions
+        for i in range(len(unknowns)):
+            arrangement = arrangement[:unknowns[i]] + perm[i] + arrangement[unknowns[i] + 1:]
+
+        contiguous = tuple(map(len, filter(len, arrangement.split('.'))))
+
+        if contiguous == groups:
+            result += 1
+
+    return result
 
 
 def part1(input: str) -> int:
-    return sum(map(arrangements, input.split("\n")))
+    return sum(map(count_arrangements_recursive, input.split("\n")))
 
 
 def part2(input: str) -> int:
@@ -50,12 +91,12 @@ class Test(unittest.TestCase):
     """)
 
     def test_part_1(self):
-        self.assertEqual(1, arrangements("???.### 1,1,3"))
-        self.assertEqual(4, arrangements(".??..??...?##. 1,1,3"))
-        self.assertEqual(1, arrangements("?#?#?#?#?#?#?#? 1,3,1,6"))
-        self.assertEqual(1, arrangements("????.#...#... 4,1,1"))
-        self.assertEqual(4, arrangements("????.######..#####. 1,6,5"))
-        self.assertEqual(10, arrangements("?###???????? 3,2,1"))
+        # self.assertEqual(1, count_arrangements_permutations("???.### 1,1,3"))
+        # self.assertEqual(4, count_arrangements_permutations(".??..??...?##. 1,1,3"))
+        # self.assertEqual(1, count_arrangements_permutations("?#?#?#?#?#?#?#? 1,3,1,6"))
+        # self.assertEqual(1, count_arrangements_permutations("????.#...#... 4,1,1"))
+        # self.assertEqual(4, count_arrangements_permutations("????.######..#####. 1,6,5"))
+        # self.assertEqual(10, count_arrangements_permutations("?###???????? 3,2,1"))
 
         self.assertEqual(21, part1(Test.example1))
 
