b01643128272ff88d7f2c4b286580d5957b380d5
Mike Fisher <mfisher@gocardless.com>
2025-01-02 09:42:10 +0000

    2024 day15 part2 wip

--- 2024/day15/day15.py
+++ 2024/day15/day15.py
@@ -65,8 +65,70 @@ def part1(input: str, print_moves=False, print_finished=False) -> int:
 
     return result
 
-def part2(input: str) -> int:
-    return 0
+def part2(input: str, print_wider_warehouse=False, print_moves=False, print_finished=False) -> int:
+    warehouse, instructions = input.split("\n\n")
+    warehouse = warehouse.splitlines()
+    instructions = reduce(lambda result, line: result + list(line), instructions.splitlines(), [])
+
+    walls = set()
+    boxes = set()
+    robot = (0, 0)
+
+    for y, row in enumerate(warehouse):
+        x = 0
+        for char in row:
+            if char == '#':
+                walls.add((y, x))
+                walls.add((y, x + 1))
+            if char == 'O':
+                boxes.add((y, x))
+            if char == '@':
+                robot = (y, x)
+
+            x += 2
+
+    if print_wider_warehouse:
+        print(
+            "\n".join("".join(
+                '##' if (y, x) in walls else
+                '[]' if (y, x) in boxes else
+                '@.' if (y, x) == robot else
+                '..' for x in range(0, len(warehouse[y]) * 2, 2)
+            ) for y in range(len(warehouse))),
+            "\n"
+        )
+
+    y, x = robot
+
+    for instruction in instructions:
+        dy, dx = moves[instruction]
+        y1, x1 = y + dy, x + dx
+
+        if (y1, x1) not in walls and (y1, x1) not in boxes and (y1, x1 - 1) not in boxes:
+            y, x = y1, x1
+        elif (y1, x1) not in walls:
+            if instruction == '>':
+                while (y1, x1) in boxes:
+                    boxes.remove((y1, x1))
+                    y1, x1 = y1 + dy * 2, x1 + dx * 2
+                    boxes.add((y1, x1))
+                    y, x = y1, x1
+
+            if instruction == '<':
+                while (y1, x1 - 1) in boxes:
+                    boxes.remove((y1, x1 - 1))
+                    y1, x1 = y1 + dy * 2, x1 + dx * 2
+                    boxes.add((y1, x1))
+                    y, x = y1, x1
+
+
+
+
+
+    result = 0
+
+
+    return result
 
 
 class Test(unittest.TestCase):
@@ -107,6 +169,18 @@ vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
 v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
     """.strip()
 
+    example3 = """
+#######
+#...#.#
+#.....#
+#..OO@#
+#..O..#
+#.....#
+#######
+
+<vv<<^^<<^^
+    """.strip()
+
     def test_part_1(self):
         self.assertEqual(2028, part1(self.example1))
         self.assertEqual(10092, part1(self.example2, print_finished=True))
@@ -114,4 +188,5 @@ v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
         print(part1(Path('./input.txt').read_text()))
 
     def test_part_2(self):
-        print(part2(Path('./input.txt').read_text()))
+        part2(self.example3, print_wider_warehouse=True)
+        # print(part2(Path('./input.txt').read_text()))
